340133d26250ed8f47b40280e172481d6f0dbbd4
Evan Tolorio <evantolorio@gmail.com>
2022-06-29 14:39:41 +0800

    feat: add initial maya webhooks

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('give')->group(function () {
+    Route::post('/success', function () {
+        return response('OK', 200);
+    });
+
+    Route::post('/failure', function () {
+        return response('OK', 200);
+    });
+
+    Route::post('/cancel', function () {
+        return response('OK', 200);
+    });
 });
